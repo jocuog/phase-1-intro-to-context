303b253a89e12ea82e3ebe29661e0c628fdd44d2
Johnathan <jocuog@gmail.com>
2022-04-03 09:32:55 -0600

    commit tree d6bfd979891b2939f1a523ef316425bf7725f8f5

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code here
+
+//build a time card fro employees that has their first and last name and their pay rate.
+    // the time card will keep trac of their clock in and clock out times in a 24 hour clock (1300 = 1pm)
+    // time stamps will be created as strings in the form: "YYYY-MM-DD time"
+
+
+//function creat
